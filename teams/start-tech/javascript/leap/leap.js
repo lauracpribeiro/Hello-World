8f38e0ff785084522f9c5507f77d751c53cfce72
laura.pereira <laura.pereira@ambevtech.com.br>
2021-08-19 21:17:57 -0300

    exercício leap

--- teams/start-tech/javascript/leap/leap.js
+++ teams/start-tech/javascript/leap/leap.js
@@ -3,16 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const isLeap = (ano) => {  //trabalhar com if o que vc quer que rode vai estar entre chave -> verdadeiro ou falso. else abre chave de volta e falso
-
-
-
-
-
-
-
-  //ano % 4 === 0
-
-
-return false;
-};
+export const isLeap = (year) => {
+  if ((!(year % 4) && (year % 100)) || !(year % 400)){
+    return true
+  } else {
+    return false}
+  };
