05db1a3d919a55135419805350fac9d3af122db7
laura.pereira <laura.pereira@ambevtech.com.br>
2021-08-19 21:09:41 -0300

    alterando o two-fer

---
+++ teams/start-tech/javascript/leap/leap.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isLeap = (ano) => {  //trabalhar com if o que vc quer que rode vai estar entre chave -> verdadeiro ou falso. else abre chave de volta e falso
+
+
+
+
+
+
+
+  //ano % 4 === 0
+
+
+return false;
+};
